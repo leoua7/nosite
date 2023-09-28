099e45739c97815a2c46bc0757b4f7e11546689e
leoua7 <leoua7@163.com>
2023-09-28 14:32:17 +0800

    show search topbar

--- worker.js
+++ worker.js
@@ -23,9 +23,9 @@ const GOOGLE_FONT = '';
 const CUSTOM_SCRIPT = ``;
 const CUSTOM_CSS = `
 <style>
-// .notion-frame {
-//   background: linear-gradient(#DED6C7 0%, #F0F0F0 100%) !important;
-// }
+  /* .notion-frame {
+    background: linear-gradient(#DED6C7 0%, #F0F0F0 100%) !important;
+  } */
 </style>
 `;
 
@@ -177,11 +177,11 @@ class HeadRewriter {
       });
     }
     element.append(`<style>
-    div.notion-topbar > div > div:nth-child(3) { display: none !important; }
+    /* div.notion-topbar > div > div:nth-child(3) { display: none !important; } */
     div.notion-topbar > div > div:nth-child(4) { display: none !important; }
     div.notion-topbar > div > div:nth-child(5) { display: none !important; }
     div.notion-topbar > div > div:nth-child(6) { display: none !important; }
-    div.notion-topbar-mobile > div:nth-child(3) { display: none !important; }
+    /* div.notion-topbar-mobile > div:nth-child(3) { display: none !important; } */
     div.notion-topbar-mobile > div:nth-child(4) { display: none !important; }
     div.notion-topbar > div > div:nth-child(1n).toggle-mode { display: block !important; }
     div.notion-topbar-mobile > div:nth-child(1n).toggle-mode { display: block !important; }
